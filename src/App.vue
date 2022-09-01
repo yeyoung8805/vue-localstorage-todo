60977773583ac0f327a17e43409a3169504dd271
yeyoung8805 <yyk8805@naver.com>
2022-09-01 11:05:54 +0900

    add h2, input and v-model="name

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return (a.createdAt = b.createdAt);
+  })
+);
 </script>
 
 <template>
-  <div>
-    Hello, World
-  </div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
