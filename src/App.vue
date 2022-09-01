44f4ac6eebfc3eaf900a157a5c7ed73486ee7c65
yeyoung8805 <yyk8805@naver.com>
2022-09-01 11:18:23 +0900

    check input_content is well applied

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ const todos_asc = computed(() =>
   })
 );
 
+const addTodo = () => {};
+
 watch(name, (newVal) => {
   localStorage.setItem("name", newVal);
 });
@@ -28,6 +30,18 @@ onMounted(() => {
       <h2 class="title">
         What's up, <input type="text" placeholder="Name here" v-model="name" />
       </h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+        {{ input_content }}
+      </form>
     </section>
   </main>
 </template>
