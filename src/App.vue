5dd9139d894234ad4d1bb53147a0bcc6b97e2d50
yeyoung8805 <yyk8805@naver.com>
2022-09-01 13:36:40 +0900

    set todos in localStorage

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,21 @@ const addTodo = () => {
   if (input_content.value.trim() === "" || input_category.value === null) {
     return;
   }
-  console.log("add Todo");
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
 };
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
 
 watch(name, (newVal) => {
   localStorage.setItem("name", newVal);
