9f5b6872b4c9de2f348ae457626bfa7ef44f6761
yeyoung8805 <yyk8805@naver.com>
2022-09-01 11:26:30 +0900

    add category radio button (business, personal)

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,30 @@ onMounted(() => {
           placeholder="e.g. make a video"
           v-model="input_content"
         />
-        {{ input_content }}
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+          {{ input_category }}
+        </div>
       </form>
     </section>
   </main>
